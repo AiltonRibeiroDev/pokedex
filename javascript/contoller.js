bc654fd7ba9a404f136c3e01b9656877c424248a
ailtonribeiro <ribeiroailtonalves@gmail.com>
2023-03-01 19:21:37 -0300

    adiocionando projeto ao github

---
+++ javascript/contoller.js
@@ -0,0 +1,57 @@
+
+
+function pikachu(){
+  document.getElementById('nome').innerHTML = 'Pikachu';
+  document.getElementById('hp').innerHTML = 'HP: 108';
+  document.getElementById('atak').innerHTML = 'Ataque: 200';
+  document.getElementById('defesa').innerHTML = 'Defesa: 300';
+  document.getElementById('velocit').innerHTML = 'Velocidade: 330';
+  document.getElementById('total').innerHTML = 'Total: 938';
+  document.getElementById('ab1').innerHTML = 'Choque do Trovão';
+  document.getElementById('ab2').innerHTML = 'Cabeçada';
+  document.getElementById('img').src='img/Pikachu-logo.png';
+  document.getElementById('style').style.backgroundColor = 'goldenrod';
+
+}
+
+function onix(){
+  document.getElementById('nome').innerHTML = 'Ônix';
+  document.getElementById('hp').innerHTML = 'HP: 120';
+  document.getElementById('atak').innerHTML = 'Ataque: 400';
+  document.getElementById('defesa').innerHTML = 'Defesa: 300';
+  document.getElementById('velocit').innerHTML = 'Velocidade: 100';
+  document.getElementById('total').innerHTML = 'Total: 920';
+  document.getElementById('ab1').innerHTML = 'Chuva de pedra';
+  document.getElementById('ab2').innerHTML = 'Esmagar';
+  document.getElementById('img').src='img/onix.png';
+  document.getElementById('style').style.backgroundColor = 'rgb(85, 85, 85)';
+
+}
+
+function bulbasuar(){
+  document.getElementById('nome').innerHTML = 'Bulbasuar';
+  document.getElementById('hp').innerHTML = 'HP: 105';
+  document.getElementById('atak').innerHTML = 'Ataque: 250';
+  document.getElementById('defesa').innerHTML = 'Defesa: 300';
+  document.getElementById('velocit').innerHTML = 'Velocidade: 260';
+  document.getElementById('total').innerHTML = 'Total: 910';
+  document.getElementById('ab1').innerHTML = 'Chuva Fervente';
+  document.getElementById('ab2').innerHTML = 'Cabeçada';
+  document.getElementById('img').src='img/bulbasuar.png';
+  document.getElementById('style').style.backgroundColor = 'green';
+
+}
+
+function charmander(){
+  document.getElementById('nome').innerHTML = 'Charmander';
+  document.getElementById('hp').innerHTML = 'HP: 120';
+  document.getElementById('atak').innerHTML = 'Ataque: 200';
+  document.getElementById('defesa').innerHTML = 'Defesa: 300';
+  document.getElementById('velocit').innerHTML = 'Velocidade: 280';
+  document.getElementById('total').innerHTML = 'Total: 900';
+  document.getElementById('ab1').innerHTML = 'Coluna de Fogo';
+  document.getElementById('ab2').innerHTML = 'Garras Cortantes';
+  document.getElementById('img').src='img/charmander.png';
+  document.getElementById('style').style.backgroundColor = 'rgb(209, 60, 6)';
+
+}
